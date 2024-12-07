56ff2ebedd7f8b50cfdb04ccb33b8ffebe51efe4
Hasibul Hasan <hasibul277127@gmail.com>
2024-12-07 17:37:06 +0600

    added the functionality to send form data in db

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app= express()
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port =process.env.PORT || 5000;
 
@@ -13,7 +14,7 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://Assignment-10-user:JXftFADpdrv7PmYY@cluster0.cojxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cojxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,12 +29,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const formCollection=client.db("formDB").collection("formCollection")
+
+    app.post("/form",async(req,res)=>{
+        const form=req.body;
+        const result = await formCollection.insertOne(form);
+        res.send(result)
+        console.log(form)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
